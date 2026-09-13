29d81031a5d0de66bb883bbd2efbe2215e53bd9d
Daniel McFeeters <fiforms@gmail.com>
2026-09-13 12:33:01 -0400

    Added missing programs to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-pip install kokoro-onnx soundfile ebooklib beautifulsoup4
+sudo apt install ocrmypdf libgl1 poppler-utils python3-venv python3-pip
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install kokoro-onnx soundfile ebooklib beautifulsoup4 opencv-python pdf2image pypdf
 wget https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/voices-v1.0.bin
 wget https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/kokoro-v1.0.onnx
